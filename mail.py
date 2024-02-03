39419ab2eba34d3d9c29e44149263480da839955
Kha Pham <49766423+Mono98ui@users.noreply.github.com>
2024-02-03 17:23:39 -0500

    send mails to multiple user

--- mail.py
+++ mail.py
@@ -1,14 +1,15 @@
 import smtplib, ssl
+import random
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from os import listdir
 from os.path import isfile, join
 
-def sendMail(title, newMessage):   
+def sendMail(title, newMessage, mailReceiver):   
   port = 465 
   smtp_server = "smtp.gmail.com"
   sender_email = "durablemail78@gmail.com" 
-  receiver_email = "phamkhapanda@gmail.com" 
+  receiver_email = mailReceiver 
   password = "lgdb zicv agcl jzfc"
 
   message = MIMEMultipart("alternative")
@@ -46,11 +47,26 @@ def readArticle(filePath):
 
   return title,text
 
+def sendArticles(pathArticles, mailReceiver):
 
-path = ".\\articles"
+  listDirPath = listdir(pathArticles)
 
-for f in listdir(path):
-  if isfile(join(path, f)):
-    print(join(path, f))
-    title, text=readArticle(join(path, f))
-    sendMail(title, text)
+  if(len(listDirPath) >= 1):
+    index = random.randrange(0, len(listDirPath))
+
+    if isfile(join(pathArticles, listDirPath[index])):
+      print(join(pathArticles, listDirPath[index]))
+      title, text=readArticle(join(pathArticles, listDirPath[index]))
+      sendMail(title, text, mailReceiver)
+
+pathArticles = ".\\articles"
+pathMailList = ".\\emailList.txt"
+
+fileEmail = open(pathMailList, "r")
+
+for mail in fileEmail:
+  if(mail.strip() != ""):
+    print(mail.strip())
+    sendArticles(pathArticles, mail.strip())
+
+fileEmail.close()
