3d0a6e2dc4d2b5b076c799aed62dd53f4a99b9af
Kha Pham <49766423+Mono98ui@users.noreply.github.com>
2024-02-03 16:47:35 -0500

    send article

--- mail.py
+++ mail.py
@@ -1,32 +1,56 @@
 import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from os import listdir
+from os.path import isfile, join
 
-port = 465 
-smtp_server = "smtp.gmail.com"
-sender_email = "durablemail78@gmail.com" 
-receiver_email = "phamkhapanda@gmail.com" 
-password = "lgdb zicv agcl jzfc"
+def sendMail(title, newMessage):   
+  port = 465 
+  smtp_server = "smtp.gmail.com"
+  sender_email = "durablemail78@gmail.com" 
+  receiver_email = "phamkhapanda@gmail.com" 
+  password = "lgdb zicv agcl jzfc"
 
-message = MIMEMultipart("alternative")
-message["Subject"] = "Nouvelle DurableMail"
-message["From"] = sender_email
-message["To"] = receiver_email
-
-newMessage = "Hello world"
-
-text = """\
-Bonjour,
-"""+newMessage
+  message = MIMEMultipart("alternative")
+  message["Subject"] = "Nouvelle DurableMail - "+title
+  message["From"] = sender_email
+  message["To"] = receiver_email
 
 
-partText = MIMEText(text, "plain")
+  text = newMessage
 
-message.attach(partText)
 
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(
-        sender_email, receiver_email, message.as_string()
-    )
+  partText = MIMEText(text, "plain")
+
+  message.attach(partText)
+
+  context = ssl.create_default_context()
+  with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+      server.login(sender_email, password)
+      server.sendmail(
+          sender_email, receiver_email, message.as_string()
+      )
+
+
+def readArticle(filePath):
+  file = open(filePath, "r")
+  text = ""
+  isTitle = True
+  title = ""
+
+  for line in file:
+    if isTitle:
+      title = line
+      isTitle = False
+    text+=line
+
+  return title,text
+
+
+path = ".\\articles"
+
+for f in listdir(path):
+  if isfile(join(path, f)):
+    print(join(path, f))
+    title, text=readArticle(join(path, f))
+    sendMail(title, text)
